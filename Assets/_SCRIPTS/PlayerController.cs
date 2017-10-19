dd840570c574001edd7d3449a2e4551d3248bc04
JakedPotato <greg.neale@hotmail.com>
2017-10-19 22:21:50 +0100

    Added first person controller

---
+++ Assets/_SCRIPTS/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10.0F;
+
+    public int forceConst = 50; //Force which is applied to the rigidbody when jumping
+    
+    private bool canJump;
+    private Rigidbody selfRigidBody;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        selfRigidBody = GetComponent<Rigidbody>();
+	}
+
+    void FixedUpdate()
+    {
+        if (canJump)
+        {
+            canJump = false;
+            selfRigidBody.AddForce(0, forceConst, 0, ForceMode.Impulse);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float jumpSpeed = 100;
+        float translation = Input.GetAxis("Vertical") * speed;
+        float strafe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        strafe *= Time.deltaTime;
+
+        transform.Translate(strafe, 0, translation);
+
+        if (Input.GetKeyUp(KeyCode.Space)) //if space isn't being pressed, allows the player to jump
+        {
+            canJump = true;
+        }
+
+        if (Input.GetKeyDown("escape"))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }                
+            else if (Cursor.lockState == CursorLockMode.None)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }    
+        }		
+	}
+}
