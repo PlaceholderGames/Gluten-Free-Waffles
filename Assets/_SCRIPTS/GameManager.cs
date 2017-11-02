d4a322067aa334242d4fa6abf9e257f4598339e2
WilliamAkins <w.akins111@gmail.com>
2017-11-02 17:23:36 +0000

    started the game manager

---
+++ Assets/_SCRIPTS/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    //The game states
+    public enum GameState {MAIN_MENU, GAME, PAUSE};
+
+    void Awake()
+    {
+
+    }
+
+	void Start () {
+		
+	}
+
+    void LoadLevel()
+    {
+
+    }
+}
