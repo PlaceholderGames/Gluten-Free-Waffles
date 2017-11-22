c30cdd44709b9dd53fb64ed48ad696a526280754
SeanBrennan645 <16020316@students.southwales.ac.uk>
2017-11-22 12:09:34 +0000

    initial commit

---
+++ Assets/_SCRIPTS/PickupDrop.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupDrop : MonoBehaviour
+{
+    //setting max distance player can pick up items from
+    public float itemRange;
+    public Transform player;
+    //used to reference which item if any should be activated on the camera
+    int itemOut = 0;//0=no item, 1=stick, 2-rock, etc 
+    //used to store name of which item is currently being held
+    string currentItem;
+    //distance dropped objects drop from player
+    public float spawnDistance;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player input to try and pick up an item
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //checking if player is currently holding a item
+            if (itemOut == 0)
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit, itemRange))
+                {
+                    //checking that item trying to be picked up is tagged to be held
+                    if (hit.transform.tag == "item")
+                    {
+                        //setting the objects rigid body and turning off collisions
+                        Rigidbody rb;
+                        rb = hit.transform.GetComponent<Rigidbody>();
+                        rb.isKinematic = false;
+                        rb.detectCollisions = false;
+                        rb.useGravity = false;
+                            //setting object as a child and giving new position
+                        hit.transform.SetParent(player, false);
+                        hit.transform.position = new Vector3(1f, 0.5f, 1f) + hit.transform.parent.position;
+                        /*OLD VERSION OF PICKING ITEMS UP
+                        //ITEMS BEING PICK UP   
+                        //checking which object has been hit
+                        if (hit.transform.gameObject.name.Contains("stick"))
+                        {
+                            //removing hit object from the scene
+                            Destroy(hit.transform.gameObject);
+                            //setting the item to be held as active
+                            transform.Find("stick").gameObject.SetActive(true);
+                            //setting which item it currently being held
+                            itemOut = 1;
+                        }
+                        else if (hit.transform.gameObject.name.Contains("rock"))
+                        {
+                            //removing hit object from the scene
+                            Destroy(hit.transform.gameObject);
+                            //setting the item to be held as active
+                            transform.Find("rock").gameObject.SetActive(true);
+                            //setting which item it currently being held
+                            itemOut = 2;
+                        }*/
+                    }
+                    //if player is trying to drop item raycast should return as null
+                }
+            }
+        }
+    }
+}
+            /*else
+            {
+                //ITEMS BEING DROPPED
+                //looking to drop a stick
+                //string ssdasd;
+                if (itemOut == 1)
+                {
+                    //setting the currently held item to false
+                    //ssdasd = arraywtvtf[itemOut];
+                    transform.Find("stick").gameObject.SetActive(false);
+                    //spawning in the dropped item
+                    dropItem(itemOut);
+                    //reseting item held to 0
+                    itemOut = 0;
+                }
+                //looking to drop a rock
+                else if (itemOut == 2)
+                {
+                    //setting the currently held item to false
+                    transform.Find("rock").gameObject.SetActive(false);
+                    //spawning in the dropped item
+                    dropItem(itemOut);
+                    //reseting item held to 0
+                    itemOut = 0;
+                }
+            }
+        }
+    }
+    //player looking to drop an item
+    void dropItem(int itemRef)
+    {
+        Vector3 playerPos = player.transform.position;
+        Vector3 playerDirection = player.transform.forward;
+        Quaternion playerRotation = player.transform.rotation;
+        Vector3 spawnPos = playerPos + playerDirection * spawnDistance;
+        //dropping a stick
+        if (itemRef == 1)
+        {
+            Instantiate(stick, spawnPos, playerRotation);
+        }
+        //dropping a rock
+        else if (itemRef == 2)
+        {
+            Instantiate(rock, spawnPos, playerRotation);
+        }
+    }
+}*/
---
+++ Assets/_SCRIPTS/playerGUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerGUI : MonoBehaviour {
+    public DayNightCycle dayNightCycle;
+
+    private string txtTime = "TIME TEST";
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), formatTime(dayNightCycle.getTime()));
+        GUI.Label(new Rect(10, 30, 100, 20), "Day " + dayNightCycle.getDay().ToString());
+    }
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    private string formatTime(int currentTime)
+    {
+        string newTime;
+        int hours, mins, secs;
+        hours = currentTime / 3600;
+        currentTime %= 3600;
+        mins = currentTime / 60;
+        currentTime %= 60;
+        secs = currentTime;
+
+        newTime = hours.ToString() + "h " + mins.ToString() + "m " + secs.ToString() + "s";
+
+        return newTime;
+    }
+}
