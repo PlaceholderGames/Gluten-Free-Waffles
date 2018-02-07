f6c73bf457e38dc500152774a38736efdf8bb6d4
JackSmerdon <jacksmerdon@gmail.com>
2018-02-07 13:00:01 +0000

    Phone GUI

--- Assets/_SCRIPTS/GUI/MobilePhone.cs
+++ Assets/_SCRIPTS/GUI/MobilePhone.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class MobilePhone : MonoBehaviour {
 
+    public DayNightCycle dayNightCycle;
     public Transform player;
     public Transform playerCamera;
     private GUIStyle guiStyle = new GUIStyle();
+    private string text;
 
     // Use this for initialization
     void Start () {
         
         transform.SetParent(GameObject.FindGameObjectWithTag("MainCamera").transform);
+        dayNightCycle = GameObject.Find("Sun").GetComponent<DayNightCycle>();
 
         Vector3 worldPoint = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 0.84f, Screen.height * 0.12f, 3));
         transform.position = worldPoint;
@@ -24,4 +27,33 @@ public class MobilePhone : MonoBehaviour {
 	void Update () {
 
 	}
+    void OnGUI()
+    {
+        Vector3 phoneDimentions = this.GetComponent<Renderer>().bounds.size;
+        print(phoneDimentions);
+        var position = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        var textSize = GUI.skin.label.CalcSize(new GUIContent(text));
+        GUI.Label(new Rect(position.x, Screen.height - position.y - 495, textSize.x + 100, textSize.y + 100), formatTime(dayNightCycle.getTime()));
+    }
+
+    private string formatTime(int currentTime)
+    {
+        string newTime;
+        int hours, mins, secs;
+        hours = currentTime / 3600;
+        currentTime %= 3600;
+        mins = currentTime / 60;
+        currentTime %= 60;
+        secs = currentTime;
+
+        string hourUpdate, minuteUpdate;
+        hourUpdate = hours.ToString();
+        if (mins < 10)
+            minuteUpdate = "0" + mins.ToString();
+        else
+            minuteUpdate = mins.ToString();
+        newTime = hourUpdate + ":" + minuteUpdate;
+
+        return newTime;
+    }
 }
