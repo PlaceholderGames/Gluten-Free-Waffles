8d20b9044e97c8745932cd03b14866f50db2a31d
JackSmerdon <jacksmerdon@gmail.com>
2018-02-08 18:54:24 +0000

    Phone Updates

--- Assets/_SCRIPTS/GUI/MobilePhone.cs
+++ Assets/_SCRIPTS/GUI/MobilePhone.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MobilePhone : MonoBehaviour {
 
@@ -20,7 +21,7 @@ public class MobilePhone : MonoBehaviour {
         Vector3 worldPoint = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 0.8f, Screen.height * 0.12f, 3));
         transform.position = worldPoint;
 
-        transform.Rotate(Vector3.right, -90);
+        //transform.Rotate(Vector3.right, -90);
         transform.Rotate(Vector3.up, -110);
     }
 
@@ -32,9 +33,12 @@ public class MobilePhone : MonoBehaviour {
         //Draw time onto the phone
         Rect phoneTime = new Rect(position.x, position.y + 160, textSize.x + 100, textSize.y + 100);
         //GUI.Label phone = GUI.Label(phoneTime, formatTime(dayNightCycle.getTime()));
+        
+        transform.GetChild(2).GetComponent<Text>().text = formatHours(dayNightCycle.getTime());
+        transform.GetChild(3).GetComponent<Text>().text = formatMins(dayNightCycle.getTime());
     }
 
-    private string formatTime(int currentTime)
+    private string formatMins(int currentTime)
     {
         string newTime;
         int hours, mins, secs;
@@ -44,13 +48,31 @@ public class MobilePhone : MonoBehaviour {
         currentTime %= 60;
         secs = currentTime;
 
-        string hourUpdate, minuteUpdate;
-        hourUpdate = hours.ToString();
+        string minuteUpdate;
+       
         if (mins < 10)
             minuteUpdate = "0" + mins.ToString();
         else
             minuteUpdate = mins.ToString();
-        newTime = hourUpdate + ":" + minuteUpdate;
+        newTime = minuteUpdate;
+
+        return newTime;
+    }
+
+    private string formatHours(int currentTime)
+    {
+        string newTime;
+        int hours, mins, secs;
+        hours = currentTime / 3600;
+        currentTime %= 3600;
+        mins = currentTime / 60;
+        currentTime %= 60;
+        secs = currentTime;
+
+        string hourUpdate;
+        hourUpdate = hours.ToString();
+
+        newTime = hourUpdate + ":";
 
         return newTime;
     }
