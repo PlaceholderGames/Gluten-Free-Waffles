d624d95ec63f37c4d2cc94a900c205821ebb017c
JakedPotato <greg.neale@hotmail.com>
2018-02-21 23:55:52 +0000

    Added basic menu
    Still need to add options :)

---
+++ Assets/_SCRIPTS/GUI/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void playGame()
+    {
+        EditorSceneManager.LoadScene(EditorSceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
