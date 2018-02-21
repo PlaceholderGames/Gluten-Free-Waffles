a7f0715d93b6a501d8fe1d2c4c20b7f6acac6711
JakedPotato <greg.neale@hotmail.com>
2018-02-21 03:46:57 +0000

    Added basic marker for quest system
    Uses the UI Overlay and is very simple. For some reason Quads didn't work.

---
+++ Assets/_SCRIPTS/GUI/RotateTowardsPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowardsPlayer : MonoBehaviour {
+
+    Transform player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Character").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player != null)
+        {
+            transform.LookAt(player.position);
+        }
+	}
+}
