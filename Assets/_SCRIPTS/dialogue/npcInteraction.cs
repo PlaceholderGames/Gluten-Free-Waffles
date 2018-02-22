9b6880ca76b6af2fccbe96e8a2bcf4414db288d0
WilliamAkins <w.akins111@gmail.com>
2018-02-22 15:22:51 +0000

    added support for the text tree gui and changed how the text looks at the player

--- Assets/_SCRIPTS/dialogue/npcInteraction.cs
+++ Assets/_SCRIPTS/dialogue/npcInteraction.cs
@@ -8,13 +8,21 @@ public class npcInteraction : MonoBehaviour
     public float npcRange;
     public string inputText;
 
-    private bool showGUI = false;
+    public GUIStyle dialogueStyle;
 
-    public GameObject player;
+    private bool GUIShowing = false;
+    private Transform player;
+    private GameObject dialogueCanvas;
 
     // Use this for initialization
     void Start ()
     {
+        dialogueCanvas = transform.GetChild(1).gameObject;
+        player = GameObject.Find("Character").transform; //Stores a reference to the actual player
+
+        dialogueCanvas.SetActive(GUIShowing);
+
+        dialogueStyle.alignment = TextAnchor.MiddleCenter;
     }
 
     // Update is called once per frame
@@ -28,13 +36,30 @@ public class npcInteraction : MonoBehaviour
             if (Physics.Raycast(ray, out hit, npcRange) && hit.transform.tag == "npc")    //checks that the player is near an npc when trying to interact
             {
                 print("YOU TOUCHED THE NPC!");
-                showGUI = true;
+
+                toggleGUI();
             }
         }
+
+        //Updates the speech bubble position so it always faces the player
+        dialogueCanvas.transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, player.position - transform.position, 10.0f, 0.0f));
+        dialogueCanvas.transform.rotation = Quaternion.Euler(0.0f, dialogueCanvas.transform.eulerAngles.y + 180, dialogueCanvas.transform.eulerAngles.z);
     }
 
     void OnGUI()
     {
-        transform.GetChild(1).GetComponent<TextMesh>().text = inputText;
+        dialogueCanvas.GetComponent<TextMesh>().text = inputText;
+
+        GUI.skin.box = dialogueStyle;
+
+        GUILayout.Box("THIS IS A BOX");
+        //GUI.Box(new Rect(0, Screen.height -100, Screen.width, 100), "This is a box", dialogueStyle);
+
+    }
+
+    void toggleGUI() {
+        GUIShowing = !GUIShowing;
+
+        dialogueCanvas.SetActive(GUIShowing);
     }
 }
