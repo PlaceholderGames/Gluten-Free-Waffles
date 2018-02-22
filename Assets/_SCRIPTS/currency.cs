ed2205c8eb3d94fc8d26c7a14ac919a9f5b86229
Sean Brennan <16020316@students.southwales.ac.uk>
2018-02-22 13:55:32 +0000

    Vendor GUI
    Had to change parts of currency script to adjust for use of UI available within unity which is a far better approach than what I was originally going for, not much currently mainly just uploading in case I can't get much done before we head in for uni

--- Assets/_SCRIPTS/currency.cs
+++ Assets/_SCRIPTS/currency.cs
@@ -2,72 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Currency : MonoBehaviour
+public class currency : MonoBehaviour
 {
-    public double currency;
-    private Item item;
-    private PickupDrop range;
-    private int selection;
-    private bool transaction;
 
-    // Use this for initialization
-    void Start()
+    public static bool transaction = false;
+    public GameObject transactionUI;
+
+	// Use this for initialization
+	void Start ()
     {
-        currency = 1000.00;
-        //used to reference
-        selection = -1; //setting to minus 1 as it cant represent an item id
-        transaction = false;
-    }
-
-
-    // Update is called once per frame
-    void Update()
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
     {
-        if (Input.GetMouseButtonDown(1))
+        //lmb click
+        if (Input.GetMouseButtonDown(0))
         {
+            //checking if a vendor was selected
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //will need to create a new tag called vendor
-            if (Physics.Raycast(ray, out hit, range.itemRange) && hit.transform.tag == "vendor")
+            if (Physics.Raycast(ray, out hit, 5) && hit.transform.tag == "vendor")
             {
                 transaction = true;
-                print("Ya buying or sellin? b/s");
-                if (Input.GetKeyDown("b") && transaction == true)
+                if (transaction)
                 {
-                    buying(hit);
-                }
-                else if (Input.GetKeyDown("s") && transaction == true)
-                {
-                    selling();
+                    vendorScreen();
                 }
             }
-        }
-    }
+        }	
+	}
 
-    void buying(RaycastHit hit)
+    void vendorScreen()
     {
-        int total = hit.transform.GetComponent<VendorSupplies>().supplies.Length;
-        print(total);
-        print("Wanna buy an energy drink for 100? y/n");
-        //outputs the players current funds
-        Debug.Log(currency);
-        //player wants to buys item
-        if (Input.GetKeyDown("y") && transaction == true)
-        {
-            currency = currency - 100;
-            transaction = false;
-            //need to add code to add item to players inventory
-        }
-        //player doesnt want to buy an item
-        else if (Input.GetKeyDown("n") && transaction == true)
-        {
-            print("Okie see ya later");
-            transaction = false;
-        }
-    }
-
-    void selling()
-    {
-        print("Selling is not currently available, sorry :(");
+        //bringing up the ui and pausing the game
+        transactionUI.SetActive(true);
+        Time.timeScale = 0f;
+        //reallowing player to see and use their mouse cursor
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
