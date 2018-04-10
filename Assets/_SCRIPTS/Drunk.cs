43bc197088d0932be7e75ae73f7053e961ae5131
WilliamAkins <w.akins111@gmail.com>
2018-04-10 04:46:27 +0100

    pretty much finished a drunk system, just need to finish adding a recovery system

---
+++ Assets/_SCRIPTS/Drunk.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Drunk : MonoBehaviour
+{
+    public float reduceDrunkSpeed = 10.0f;
+    public float initialAlpha = 0.1f;
+
+    private Vitals vitals;
+    private Camera FPPCamera;
+    private Image drunkOverlay;
+    private CanvasGroup drunkOverlayGroup;
+
+    private float sensitivityX = 15.0f;
+    private float sensitivityY = 15.0f;
+
+    private float smooth = 2.0f;
+    private float smoothXAxis;
+    private float smoothYAxis;
+
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+
+    private bool fovIncrease = true;
+    private float fadeTime = 0.0f;
+
+    // Use this for initialization
+    private void Start()
+    {
+        //get some components & objects
+        GameObject player = GameObject.Find("Character");
+        vitals = player.GetComponent<Vitals>();
+        FPPCamera = player.transform.Find("FPPCamera").GetComponent<Camera>();
+        drunkOverlay = transform.Find("DrunkOverlay").GetComponent<Image>();
+        drunkOverlayGroup = drunkOverlay.GetComponent<CanvasGroup>();
+
+        //set the initial alpha value for the drunk overlay
+        drunkOverlay.color = new Color(drunkOverlay.color.r, drunkOverlay.color.g, drunkOverlay.color.b, initialAlpha);
+    }
+
+    private void Update()
+    {
+        //increases and lowers the fov over time
+        if (fovIncrease) {
+            FPPCamera.fieldOfView += 10.0f * Time.deltaTime;
+
+            if (FPPCamera.fieldOfView >= 140.0f)
+            {
+                fovIncrease = false;
+            }
+        } 
+        else
+        {
+            FPPCamera.fieldOfView -= 10.0f * Time.deltaTime;
+
+            if (FPPCamera.fieldOfView <= 120.0f)
+            {
+                fovIncrease = true;
+            }
+        }
+
+        vitals.setSoberness(vitals.getSoberness() + (1.0f * Time.deltaTime));
+
+        //lerp the alpha of the drunk panel overlay
+        float newAlpha = Mathf.Lerp(initialAlpha, 0.0f, fadeTime);
+        drunkOverlay.color = new Color(drunkOverlay.color.r, drunkOverlay.color.g, drunkOverlay.color.b, newAlpha);
+
+        //lerp the alpha of the black gradient around the camera
+        drunkOverlayGroup.alpha = Mathf.Lerp(1.0f, 0.0f, fadeTime);
+
+        fadeTime += (1.0f / reduceDrunkSpeed) * Time.deltaTime;
+
+        print("soberness = " + Mathf.Round(vitals.getSoberness()));
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate ()
+    {
+        //lerp the x and y pos of the mouse as the player moves the mouse
+        smoothXAxis = Mathf.Lerp(smoothXAxis, Input.GetAxis("Mouse X"), Time.deltaTime * smooth);
+        smoothYAxis = Mathf.Lerp(smoothYAxis, Input.GetAxis("Mouse Y"), Time.deltaTime * smooth);
+
+        rotationX += smoothXAxis * sensitivityX;
+        rotationY += smoothYAxis * sensitivityY;
+
+        FPPCamera.transform.localEulerAngles = new Vector3(-rotationY, rotationX, transform.localEulerAngles.z);
+    }
+}
--- Assets/_SCRIPTS/Vitals.cs
+++ Assets/_SCRIPTS/Vitals.cs
@@ -7,11 +7,12 @@ public class Vitals : MonoBehaviour
     [Range(1, 100)]
     public int health = 100;
     [Range(0, 100)]
-    public int soberness = 100;
+    public float soberness = 100;
     [Range(0, 100)]
     public int energy = 100;
 
     private bool playerIsDead = false;
+    private bool playerIsDrunk = false;
 
     public void setHealth(int newHealth)
     {
@@ -27,6 +28,14 @@ public class Vitals : MonoBehaviour
         return health;
     }
 
+    public void setSoberness(float newSoberness) {
+        soberness = newSoberness;
+    }
+
+    public float getSoberness() {
+        return soberness;
+    }
+
     // Use this for initialization
     private void Start ()
     {
@@ -38,11 +47,19 @@ public class Vitals : MonoBehaviour
         //debug command to instalty kill the player
         debugRemoveHP();
 
+        //debug command to instalty make the player drunk
+        debugMakeDrunk();
+
         if (health == 0 && !playerIsDead)
         {
             playerIsDead = true;
-
             GameObject knockedOutObj = Instantiate(Resources.Load("Prefabs/KnockedOut"), Vector3.zero, Quaternion.identity) as GameObject;
+        }
+
+        if (soberness <= 50 && !playerIsDrunk)
+        {
+            playerIsDrunk = true;
+            GameObject drunkObj = Instantiate(Resources.Load("Prefabs/Drunk"), Vector3.zero, Quaternion.identity) as GameObject;
         }
     }
 
@@ -54,6 +71,15 @@ public class Vitals : MonoBehaviour
             {
                 print("HP lowered to 0.");
                 health = 0;
+            }
+        }
+    }
+
+    private void debugMakeDrunk() {
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            if (Input.GetKeyDown(KeyCode.E) && !playerIsDrunk) {
+                print("Player is now drunk.");
+                soberness = 0;
             }
         }
     }
