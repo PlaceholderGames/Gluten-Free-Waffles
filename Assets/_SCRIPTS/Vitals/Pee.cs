44d891054ce7f8fe7b61be6c4be770ad13c0529d
WilliamAkins <w.akins111@gmail.com>
2018-04-16 17:49:41 +0100

    peeing is now half working

--- Assets/_SCRIPTS/Vitals/Pee.cs
+++ Assets/_SCRIPTS/Vitals/Pee.cs
@@ -6,19 +6,30 @@ public class Pee : MonoBehaviour
 {
     public float changeBladderSpeed = 30.0f; //how fast the bladder var will fill up in seconds
 
+    private GameObject player;
     private Vitals vitals;
+    private CamMouseLook camMouseLook;
 
     // Use this for initialization
     void Start ()
     {
         //get some components & objects
-        vitals = GameObject.Find("Character").GetComponent<Vitals>();
+        player = GameObject.Find("Character");
+        vitals = player.GetComponent<Vitals>();
+        camMouseLook = player.transform.Find("FPPCamera").GetComponent<CamMouseLook>();
+
         vitals.setBladderState(true);
+
+        //set the initial position to the player position
+        transform.position = player.transform.position;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        //each frame ensure the pee position is on the player
+        transform.position = player.transform.position;
+        transform.localRotation = camMouseLook.transform.localRotation;
+       // transform.localRotation.eulerAngles.Set(0.0f, camMouseLook.transform.localRotation.y, 0.0f);
+    }
 }
