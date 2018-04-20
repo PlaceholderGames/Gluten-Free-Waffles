43b129353740122051bde484bd15972264431e3c
SeanBrennan645 <16020316@students.southwales.ac.uk>
2018-04-20 18:28:30 +0100

    Added a beer vendor near the city street and increased the cost of beer item

--- Assets/_SCRIPTS/Item Storage/ItemDatabase.cs
+++ Assets/_SCRIPTS/Item Storage/ItemDatabase.cs
@@ -10,7 +10,7 @@ public class ItemDatabase : MonoBehaviour
     {
         //Add all items in the game here
         items.Add(new Item(name: "Energy Drink", id: 0, desc: "Increases\nyour speed\nfor a short\n amount of\ntime.", type: Item.ItemType.Drink, destroy: true, quantity: 0, cost: 100));
-        items.Add(new Item(name: "Premium Larger", id: 1, desc: "This will\n f%$k you up.", type: Item.ItemType.Drink, destroy: true, quantity: 0, cost: 100));
+        items.Add(new Item(name: "Premium Larger", id: 1, desc: "This will\n f%$k you up.", type: Item.ItemType.Drink, destroy: true, quantity: 0, cost: 150));
         items.Add(new Item(name: "Police Hat", id: 2, desc: "Definitely should not have this.", type: Item.ItemType.Drink, destroy: false, quantity: 0, cost: 100));
         items.Add(new Item(name: "Mobile Phone", id: 3, desc: "Mobile Phone", type: Item.ItemType.Drink, destroy: false, quantity: 0, cost: 100));
         items.Add(new Item(name: "Red T-Shirt", id: 4, desc: "Classic\nRed T-Shirt", type: Item.ItemType.Clothes, destroy: false, quantity: 0, cost: 100));
