09563a70d863d3ddd3cafc1fc67ef97dff12c6c7
JakedPotato <greg.neale@hotmail.com>
2018-04-20 17:43:46 +0100

    Fixed NPC Quest not finishing

--- Assets/_SCRIPTS/Quests/npcQuestPickup.cs
+++ Assets/_SCRIPTS/Quests/npcQuestPickup.cs
@@ -29,6 +29,7 @@ public class npcQuestPickup : BaseQuest
             {
                 activateBool();
                 updateQuest();
+                
             }
             if (hasBeenSpokenTo.beingHeld)
             {
